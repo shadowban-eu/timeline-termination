6b9672e6baa2fd8649dfa7b584cdc26fa160902b
Raphael Beer <raphael.beer@protonmail.com>
2019-11-11 18:41:02 +0100

    Refactor: class syntax for TestService

--- src/api/services/TestService.js
+++ src/api/services/TestService.js
@@ -1,27 +1,27 @@
 const GuestSession = require('./GuestSession');
 const TweetObject = require('../utils/TweetObject');
 
-const TestService = {};
+class TestService {
+  static async getTweetsForSubject(subjectTweetId) {
+    const timeline = await GuestSession.getTimeline(subjectTweetId);
+    const tweetIds = Object.keys(timeline.tweets).sort();
 
-TestService.getTweetsForSubject = async (subjectTweetId) => {
-  const timeline = await GuestSession.getTimeline(subjectTweetId);
-  const tweetIds = Object.keys(timeline.tweets).sort();
+    const subjectIdx = tweetIds.indexOf(subjectTweetId);
+    const subjectTweet = timeline.tweets[subjectTweetId];
 
-  const subjectIdx = tweetIds.indexOf(subjectTweetId);
-  const subjectTweet = timeline.tweets[subjectTweetId];
+    const testTweetId = tweetIds[subjectIdx + 1];
+    const testTweet = timeline.tweets[testTweetId];
 
-  const testTweetId = tweetIds[subjectIdx + 1];
-  const testTweet = timeline.tweets[testTweetId];
+    return {
+      subject: new TweetObject(subjectTweet),
+      testedWith: new TweetObject(testTweet)
+    };
+  }
 
-  return {
-    subject: new TweetObject(subjectTweet),
-    testedWith: new TweetObject(testTweet)
-  };
-};
-
-TestService.test = async (subjectTweetId, testTweetId) => {
-  const timeline = await GuestSession.getTimeline(testTweetId);
-  return !Object.keys(timeline.tweets).includes(subjectTweetId);
-};
+  static async test(subjectTweetId, testTweetId) {
+    const timeline = await GuestSession.getTimeline(testTweetId);
+    return !Object.keys(timeline.tweets).includes(subjectTweetId);
+  }
+}
 
 module.exports = TestService;
