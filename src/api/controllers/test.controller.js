caab76178122e1e6a29a47f8299752d7f81828ae
Raphael Beer <raphael.beer@protonmail.com>
2019-11-21 21:07:24 +0100

    Fix: return null from GET /v1/test/:tweetId, when tweet has no reply

--- src/api/controllers/test.controller.js
+++ src/api/controllers/test.controller.js
@@ -1,6 +1,12 @@
 const TestService = require('../services/TestService');
 
 module.exports.single = async (req, res) => {
-  const testCase = await TestService.test(req.params.tweetId);
-  res.json(testCase.transform());
+  try {
+    const testCase = await TestService.test(req.params.tweetId);
+    res.json(testCase.transform());
+  } catch (err) {
+    if (err instanceof RangeError) {
+      res.json(null);
+    }
+  }
 };
